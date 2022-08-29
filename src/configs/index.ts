c5bf32ddea0a850a70a3895c63cfedd0fffd799f
TWINGER-NHATHN <stu715113037@hnue.edu.vn>
2022-08-30 02:57:02 +0700

    feat: update config

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,2 +1,3 @@
-// export const baseUrl = "https://kanban-api-3000.herokuapp.com/api/v1";
-export const baseUrl = "http://localhost:5000/api/v1/expense-tracker";
+export const baseUrl =
+  "https://kanban-api-3000.herokuapp.com/api/v1/expense-tracker";
+// export const baseUrl = "http://localhost:5000/api/v1/expense-tracker";
